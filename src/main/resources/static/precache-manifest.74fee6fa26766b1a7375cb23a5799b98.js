11d2124f731e3623bf05260012edebe760b49735
Xianda Bai <xiandabai@gwu.edu>
2020-04-17 21:37:11 -0400

    bug update

---
+++ src/main/resources/static/precache-manifest.74fee6fa26766b1a7375cb23a5799b98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0412df3fbe4cdf44f45cd9b3b65e6613",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd8134a3c40706812c12",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e145699c4aec9a3aaf3c",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "dd8134a3c40706812c12",
+    "url": "/static/js/2.249558b1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.249558b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e145699c4aec9a3aaf3c",
+    "url": "/static/js/main.cff2990f.chunk.js"
+  },
+  {
+    "revision": "a9d271c153c0057002ce",
+    "url": "/static/js/runtime-main.121da3f1.js"
+  }
+]);
